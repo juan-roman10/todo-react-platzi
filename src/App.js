fb224567c8427e884130b8891b2fb5c366febe7f
Mariano Paduani <gaston.paduani15@gmail.com>
2023-02-05 19:04:45 -0300

    components created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { TodoCounter } from './components/TodoCounter'
+import { TodoSearch } from './components/TodoSearch'
+import { TodoList } from './components/TodoList'
+import { TodoItem } from './components/TodoItem'
+import { CreateTodoButton } from './components/CreateTodoButton'
 import './App.css';
+
+const todos = [
+  { text: 'Hacer algo', completed: false },
+  { text: 'Hacer algo 2', completed: false },
+  { text: 'Hacer algo 3', completed: false },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        { todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
